df33b780b615006f69a36b49129f8babcb71b641
guilleatm <47484930+guilleatm@users.noreply.github.com>
2023-01-09 17:26:01 +0100

    comentarios

--- Assets/Productor.cs
+++ Assets/Productor.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+// Necesitaremos este using para la clase UnityEvent !!
 using UnityEngine.Events;
 
 public class Productor : MonoBehaviour
 {
     [SerializeField]
+    // Hacemos el evento estático para que sea accesible más facilmente sin utilizar funciones Find()
     public static UnityEvent miEvento = new UnityEvent();
 
 
